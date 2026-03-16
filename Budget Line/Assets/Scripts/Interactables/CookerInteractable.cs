4978a21bacbb9a9b9a145fb7a89fd1aa532fce6b
Ness <146290658+IronNess@users.noreply.github.com>
2026-03-16 20:50:34 +0000

    Create CookerInteractable.cs
    Script for cooker interactable added. Allows player to interact with cooker to cook meals and decrease hunger levels

---
+++ Budget Line/Assets/Scripts/Interactables/CookerInteractable.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CookerInteractable : MonoBehaviour
+{
+    [SerializeField] private InteractionChoiceUI choiceUI;
+    [SerializeField] private GameState state;
+    [SerializeField] private TimeSystem timeSystem;
+
+    private void Awake()
+    {
+        if (!choiceUI) choiceUI = FindObjectOfType<InteractionChoiceUI>();
+        if (!state) state = FindObjectOfType<GameState>();
+        if (!timeSystem) timeSystem = FindObjectOfType<TimeSystem>();
+    }
+
+    public void Interact()
+    {
+        choiceUI.Show(
+            "Cooker",
+            "Cooking a meal takes longer, but restores much more hunger.",
+            new InteractionChoice("Cook a meal", () =>
+            {
+                state.AddHunger(+35);
+                state.AddEnergy(-2);
+                timeSystem.AdvanceTime(2);
+            }),
+            new InteractionChoice("Cancel", () => { })
+        );
+    }
+}
